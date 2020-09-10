83019b2e446fd800d079832dd6b3683abce7f00c
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2020-09-09 22:47:07 -0700

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        while (true) {
+            In file = new In("speed.txt");
+            while (!file.isEmpty()) {
+                String s = file.readLine();
+                int i = 0;
+                while (i < s.length()) {
+                    String w = "";
+                    while (i < s.length() && s.charAt(i) != ' ') {
+                        w = w + s.charAt(i);
+                        ++i;
+                    }
+                    i++;
+                    StdDraw.clear();
+                    StdDraw.text(0.5, 0.5, w);
+                    StdDraw.pause(250);
+                }
+            }
+            StdDraw.pause(500);
+        }
+    }
+}
